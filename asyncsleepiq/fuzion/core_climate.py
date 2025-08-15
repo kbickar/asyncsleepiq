83ecc15cca1e80ca49ac2cbb4db449c1b726e8ec
Aaron Morabito <aaron.p.morabito@gmail.com>
2025-08-15 17:03:23 -0400

    Wire core climate detection to bed model

--- asyncsleepiq/fuzion/core_climate.py
+++ asyncsleepiq/fuzion/core_climate.py
@@ -18,8 +18,6 @@ class SleepIQFuzionCoreClimate(SleepIQCoreClimate):
     """
 
     max_core_climate_time = 600
-    _set_mode_key = "SetHeidiMode"
-    _get_mode_key = "GetHeidiMode"
 
     async def set_mode(self, temperature: CoreTemps, time: int) -> None:
         """Set core climate state through API."""
@@ -27,13 +25,13 @@ class SleepIQFuzionCoreClimate(SleepIQCoreClimate):
             raise ValueError(f"Invalid Time, must be between 0 and {self.max_core_climate_time}")
 
         args = [SIDES_FULL[self.side].lower(), temperature.name.lower(), str(time)]
-        await self._api.bamkey(self.bed_id, self._set_mode_key, args)
+        await self._api.bamkey(self.bed_id, "SetHeidiMode", args)
         await self.update({})
 
     async def update(self, data: dict[str, Any]) -> None:
         """Update the core climate data through the API."""
         args = [SIDES_FULL[self.side].lower()]
-        data = await self._api.bamkey(self.bed_id, self._get_mode_key, args)
+        data = await self._api.bamkey(self.bed_id, "GetHeidiMode", args)
         data = data.split()
         self.temperature = CoreTemps[data[0].upper()]
         self.is_on = self.temperature > 0
@@ -43,6 +41,21 @@ class SleepIQFuzionCoreClimate(SleepIQCoreClimate):
 class SleepIQFuzionClimateCoolCoreClimate(SleepIQFuzionCoreClimate):
     """Core Climate representation for ClimateCool beds."""
 
-    _set_mode_key = "SetClimateMode"
-    _get_mode_key = "GetClimateMode"
+    async def set_mode(self, temperature: CoreTemps, time: int) -> None:
+        """Set core climate state through API."""
+        if time <= 0 or time > self.max_core_climate_time:
+            raise ValueError(f"Invalid Time, must be between 0 and {self.max_core_climate_time}")
+
+        args = [SIDES_FULL[self.side].lower(), temperature.name.lower(), str(time)]
+        await self._api.bamkey(self.bed_id, "SetClimateMode", args)
+        await self.update({})
+
+    async def update(self, data: dict[str, Any]) -> None:
+        """Update the core climate data through the API."""
+        args = [SIDES_FULL[self.side].lower()]
+        data = await self._api.bamkey(self.bed_id, "GetClimateMode", args)
+        data = data.split()
+        self.temperature = CoreTemps[data[0].upper()]
+        self.is_on = self.temperature > 0
+        self.timer = int(data[1]) if self.is_on else 0
 
